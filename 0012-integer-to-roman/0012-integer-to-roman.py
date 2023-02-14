737b4348acfa93e387e39c53417920d439fcf7e7
Selamu Dawit <88835502+selamudawitl04@users.noreply.github.com>
2023-02-14 14:03:23 +0300

    Time: 74 ms (20.05%), Space: 14 MB (28.13%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -2,18 +2,10 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         def toRoman(numDigit, num)->str:
             if num == 0 : return ''
-            print(numDigit, num)
             hashMap = {
-                1 : 'I', 4 : 'IV', 5 : 'V', 9 : 'IX', 10 : 'X', 40 : 'XL',
-                50 : 'L',
-                90 : 'XC',
-                100 : 'C',
-                400 : 'CD',
-                500 : 'D',
-                900 : 'CM',
-                1000 : 'M'
-            }
-            
+                1 : 'I', 4 : 'IV', 5 : 'V', 9 : 'IX', 10 : 'X', 40 : 'XL',50 : 'L',
+                90 : 'XC', 100 : 'C', 400 : 'CD', 500 : 'D', 900 : 'CM', 1000 : 'M'
+            }   
             if num / numDigit == 4 or num / numDigit == 9:
                 return hashMap[num][::-1]
             elif num / numDigit < 5:
@@ -23,7 +15,6 @@ class Solution:
                 print(dig)
                 return  (hashMap[numDigit * 5] + hashMap[numDigit] * (dig - 5))[::-1]
             else: return hashMap[numDigit * 5]
-        
         result = ''
         string = str(num)
         for i in range(1, len(str(num))+1):
